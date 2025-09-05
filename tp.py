9a01890fb907bd165a9e49857d49f5eb1590aa65
Sanketsingh Thakur <117128499+Sanketsingh23@users.noreply.github.com>
2025-09-05 21:18:43 +0530

    Delete tp.py

--- tp.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from youtube_comment_downloader import YoutubeCommentDownloader
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
-import matplotlib.pyplot as plt
-
-
-@st.cache_resource
-def load_model():
-    
